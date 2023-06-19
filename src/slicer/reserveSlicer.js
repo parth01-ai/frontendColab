3fb94a38e36411a123eb2a951b06ae78ef8d0f73
PorusDwinner <amit2811995@gmail.com>
2023-06-19 12:05:14 +0530

    temp. seats added to global state

--- src/slicer/reserveSlicer.js
+++ src/slicer/reserveSlicer.js
@@ -2,6 +2,13 @@ import { createSlice } from "@reduxjs/toolkit";
 
 const initialState = {
     loginState : false,
+    seats : [
+        { id: 1, seatNumber: '1AW', isReserved: false, isSelected: false },
+        { id: 2, seatNumber: '2A', isReserved: false, isSelected: false },
+        { id: 3, seatNumber: '3A', isReserved: false, isSelected: false },
+        { id: 4, seatNumber: '1BW', isReserved: false, isSelected: false },
+        { id: 5, seatNumber: '2B', isReserved: false, isSelected: false },
+    ]
 };
 
 export const reserveSlicer = createSlice({
